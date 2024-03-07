df99caaf7be7ed4393c36be9143f8e111e39607a
Demidog03 <olzhasktl362@gmail.com>
2024-03-07 20:57:36 +0600

    lesson19

--- javascript/lesson19/script.js
+++ javascript/lesson19/script.js
@@ -156,23 +156,140 @@
 
 
 // ЗАДАНИЕ 3
-const promptNum1 = +prompt('Введите первое число: ')
-const promptNum2 = +prompt('Введите второе число: ')
+// const promptNum1 = +prompt('Введите первое число: ')
+// const promptNum2 = +prompt('Введите второе число: ')
 
-class Calculator {
-    constructor(num1, num2) {
-        this.num1 = num1
-        this.num2 = num2
+// class Calculator {
+//     constructor(num1, num2) {
+//         this.num1 = num1
+//         this.num2 = num2
+//     }
+
+//     sum() {
+//         console.log(this.num1 + this.num2)
+//     }
+//     mul() {
+//         console.log(this.num1 * this.num2)
+//     }
+// }
+
+// const calculator1 = new Calculator(promptNum1, promptNum2)
+// calculator1.sum()
+// calculator1.mul()
+
+
+// ЗАДАНИЕ 4
+class Logo {
+    top = 0;
+    left = 0;
+    width = 200;
+    height = 200;
+    element = null;
+    constructor(url) {
+      this.imgUrl = url;
+    }
+  
+    init() {
+      // метод должен создать тег img
+      const img = document.createElement('img')
+      // вложить в него src картинки (this.imgUrl)
+      img.src = this.imgUrl
+      // и записать в this.html
+      this.element = img
+      // + дергаем render
+      this.render()
+      // + странице должен залится фон черным
+      document.body.style.backgroundColor = 'black'
+    }
+    
+    render() {
+      // метод должен отрисовать изображение (this.element) на странице
+      document.body.appendChild(this.element)
+      // применить фиксированное позиционирование
+      this.element.style.position = 'fixed'
+      // использовать this.top и this.left для указания позиции
+      this.element.style.top = `${this.top}px`
+      // img {
+      //    top: 0
+      // }
+      this.element.style.left = `${this.left}px`
+      // использовать this.width для указания ширины
+      this.element.width = this.width
+      // использовать this.height для указания высоту
+      this.element.height = this.height
+      // object-fit: cover
+      this.element.style.objectFit = 'cover'
+      // округляем this.element
+      this.element.style.borderRadius = '50%'
+    }
+  
+    moveUp() {
+      // метод должен изменять top нашего объекта так
+      // чтобы элемент передвинулся ВЫШЕ
+      // на 20px
+      this.top -= 20
+      // + дергаем render
+      this.render()
+    }
+    moveDown() {
+      // метод должен изменять top нашего объекта так
+      // чтобы элемент передвинулся НИЖЕ
+      // на 20px
+      this.top += 20
+      // + дергаем render
+      this.render()
+    }
+    moveLeft() {
+      // метод должен изменять left нашего объекта так
+      // чтобы элемент передвинулся ЛЕВЕЕ
+      // на 20px
+      this.left -= 20
+      // + дергаем render
+      this.render()
+    }
+    moveRight() {
+      // метод должен изменять left нашего объекта так
+      // чтобы элемент передвинулся ПРАВЕЕ
+      // на 20px
+      this.left += 20
+      // + дергаем render
+    }
+  }
+  
+const logo1 = new Logo('https://cs13.pikabu.ru/post_img/big/2023/09/11/5/1694416670162565263.jpg')
+const logo2 = new Logo('https://trikky.ru/wp-content/blogs.dir/1/files/2020/04/22/1564314090_3.jpg')
+logo2.left = 250
+
+
+
+// size = {
+//     width: 200
+//     height: 200
+// }
+
+class Circle {
+    element = null
+    constructor(size, color) {
+        this.size = size
+        this.color = color
     }
 
-    sum() {
-        console.log(this.num1 + this.num2)
+    init() {
+        const circle = document.createElement('div')
+        circle.style.backgroundColor = this.color
+        circle.style.width = `${this.size.width}px`
+        circle.style.height = `${this.size.height}px`
+        circle.style.borderRadius = '50%'
+        this.element = circle
+        this.render()
     }
-    mul() {
-        console.log(this.num1 * this.num2)
+
+    render() {
+        document.body.appendChild(this.element)
     }
 }
 
-const calculator1 = new Calculator(promptNum1, promptNum2)
-calculator1.sum()
-calculator1.mul()
+const circle1 = new Circle({width: 200, height: 200}, 'red')
+const circle2 = new Circle({width: 400, height: 300}, 'green')
+circle1.init()
+circle2.init()
